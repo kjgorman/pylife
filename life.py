703effa67777e22e201b47018c65ef1adefd8bef
kjgorman <kieran.j.gorman@gmail.com>
2012-07-29 20:10:21 +1200

    Changed life to be a class so I can unit test the window dimensions. Made the windows dimensions fail outright rather than fall back to valid inputs

--- life.py
+++ life.py
@@ -7,52 +7,186 @@ import time
 import math
 from sys import argv
 
-#Sets up the curses window
-def startup():
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(1)
-#Destructs the curses window and reverts key settings
-def shutdown():
-    curses.echo()
-    curses.nocbreak()
-    stdscr.keypad(0)
-    curses.endwin()
 
-#Fills in the basic background field
-#TODO: only update affected cells per tick
-def background(gen, nodelay):
-    for y in xrange(0,height):
-        for x in range(0,width):
-            try:
-                #the horizontal sides have pipes
-                if (x == 0) | (x == width-1):
-                    stdscr.addch( y, x, ord('|'))
-                #the vertical sides have hypens
-                elif (y == 0) | (y == height-1):
-                    stdscr.addch( y, x, ord('-'))
-                #and blank space are periods
-                else:
-                    stdscr.addch( y, x, ord('.') )
-                stdscr.addstr(height, 1, "gen: "+str(gen))
-                stdscr.addstr(height, 10, "[r]un [s]tep [n]ew [d]el [q]uit")
-                if nodelay:
-                    stdscr.addstr(height, 42, "[running]")
-                else:
-                    stdscr.addstr(height, 42, "[paused] ")
-            except curses.error:
-                shutdown()
-                raise Exception("Curses broke, probably your width or height is too large for the visible screen")
+class Life:
+
+   
+
+    def __init__(self, width, height):
+        self.ux = 0
+        self.uy = 0
+        self.width, self.height = self.setupWindowSize(width, height)
+        
+    def run(self):
+        self.stdscr = curses.initscr()
+        self.startup()
+        self.cells = []
+        self.generation = 0
+        self.nodelay = 0
+        while 1:
+            self.background(self.generation, self.nodelay)
+            self.fillCells(self.cells)
+            self.mv_term_cursor(self.ux, self.uy)
+            #by default a blocking function
+            #when [r] is pressed we move to
+            #a timeout period of a tenth of 
+            #a second for animation. 
+            self.c = self.stdscr.getch()
+
+            if self.c == ord('s') or self.nodelay:
+                self.cells = self.step(self.cells)
+                self.generation = self.generation+1
+            if self.c == ord('r'):
+                if self.nodelay: self.stdscr.timeout(-1) 
+                else:            self.stdscr.timeout(75)
+                self.nodelay = self.nodelay ^ 1
+            elif self.c == ord('n'):         self.cells = self.addCell(self.ux, self.uy, self.cells)
+            elif self.c == ord('d'):         self.cells = self.delCell(self.ux, self.uy, self.cells)
+            elif self.c == curses.KEY_RIGHT: self.ux = self.ux + 1 if self.ux < self.width-1 else self.ux
+            elif self.c == curses.KEY_LEFT:  self.ux = self.ux - 1 if self.ux > 0 else self.ux
+            elif self.c == curses.KEY_UP:    self.uy = self.uy - 1 if self.uy > 0 else self.uy
+            elif self.c == curses.KEY_DOWN:  self.uy = self.uy + 1 if self.uy < self.height-1 else self.uy
+            elif self.c == ord('q'):
+                break
+        self.shutdown()
+
+    #Sets up the curses window
+    def startup(self):
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(1)
+    #Destructs the curses window and reverts key settings
+    def shutdown(self):
+        curses.echo()
+        curses.nocbreak()
+        self.stdscr.keypad(0)
+        curses.endwin()
+
+    #Fills in the basic background field
+    #TODO: only update affected cells per tick
+    def background(self, gen, nodelay):
+        for y in xrange(0,self.height):
+            for x in range(0,self.width):
+                try:
+                    #the horizontal sides have pipes
+                    if (x == 0) | (x == self.width-1):
+                        self.stdscr.addch( y, x, ord('|'))
+                    #the vertical sides have hypens
+                    elif (y == 0) | (y == self.height-1):
+                        self.stdscr.addch( y, x, ord('-'))
+                    #and blank space are periods
+                    else:
+                        self.stdscr.addch( y, x, ord('.') )
+                    self.stdscr.addstr(self.height, 1, "gen: "+str(gen))
+                    self.stdscr.addstr(self.height, 10, "[r]un [s]tep [n]ew [d]el [q]uit")
+                    if self.nodelay:
+                        self.stdscr.addstr(self.height, 42, "[running]")
+                    else:
+                        self.stdscr.addstr(self.height, 42, "[paused] ")
+                except curses.error:
+                    self.shutdown()
+                    raise Exception("Curses broke, probably your width or height is too large for the visible screen")
     
-    #after drawing refresh the window
-    stdscr.refresh()
+        #after drawing refresh the window
+        self.stdscr.refresh()
 
-#Iterates the collection of active cells and
-#'fills' them into the field
-def fillCells(cells):
-    for cell in cells:
-        cell.fill()
-    stdscr.refresh()
+    #Iterates the collection of active cells and
+    #'fills' them into the field
+    def fillCells(self, cells):
+        for cell in cells:
+            cell.fill(self.stdscr)
+        self.stdscr.refresh()
+
+    #Adding a cell will ensure the set condition
+    #of disallowing duplicates
+    def addCell(self, x,y,cells):
+        for cell in cells:
+            if cell.x == x and cell.y == y:
+                return cells
+        cells.append(Cell(x,y))
+        return cells
+
+    def delCell(self, x,y,cells):
+        for cell in cells:
+            if cell.x == x and cell.y == y:
+                cells.remove(cell)
+                return cells
+        return cells
+
+    #Finds adjacent alive cells
+    def adjacentCells(self, cell, cells):
+        adj = []
+        for other in cells:
+            dx = math.fabs(cell.x-other.x)
+            dy = math.fabs(cell.y-other.y)
+            if (dx <= 1) & (dy <= 1): adj.append(other)
+        for other in adj:
+            if other.x == cell.x and other.y == cell.y:
+                adj.remove(other)
+        return adj
+
+    #Finds adjacent dead points
+    #TODO: This could really just be surrounding elements not
+    #      in adjacentCells
+    def adjacentPoints(self, cell, cells):
+        dead = []
+        candidates =[(x,y) 
+                    for x in [cell.x-1, cell.x, cell.x+1] if (x > 0) & (x < self.width-1)
+                    for y in [cell.y-1, cell.y, cell.y+1] if (y > 0) & (y < self.height-1)
+                    if (x,y) != (cell.x, cell.y)
+                    ]
+        cellPos = []
+        for other in cells:
+             cellPos.append((other.x, other.y))
+        dead = self.difference(candidates, cellPos)
+        return dead  
+  
+    #The set difference of two lists a nd b
+    #(assuming the lists meet set conditions of
+    # uniqueness)
+    def difference(self, a,b):
+        b = set(b)
+        return [aa for aa in a if aa not in b]            
+        
+    #The main portion of life, a single step through the
+    #reproduction routine
+    def step(self, cells):
+        dead = []
+        toRemove = []
+        toAdd = []
+        for cell in cells:
+            dead.extend(self.adjacentPoints(cell, cells))
+        for (x,y) in dead:
+            candidate = Cell(x,y)
+            adjs = self.adjacentCells(candidate, cells)
+            bordering = len(adjs)
+            if bordering == 3: toAdd.append(candidate)
+        for cell in cells:
+            adjs = self.adjacentCells(cell, cells)
+            bordering = len(adjs)
+            if (bordering < 2) or (bordering > 3): toRemove.append(cell)
+        for cell in toRemove:
+            cells.remove(cell)
+        for cell in set(toAdd):
+            cells = self.addCell(cell.x, cell.y, cells)
+        self.fillCells(cells) 
+        return cells 
+          
+    #Used for manipulating the terminal cursor for cell placement
+    def mv_term_cursor(self, ux, uy):
+            self.stdscr.move(uy,ux)
+
+    def setupWindowSize(self, w, h):
+        width = 50
+        height = 30
+        try:
+            width = int(w)
+            height = int(h)
+        except:
+            raise Exception("Failed to interpret arguments as integers")
+        if width < 5 or height < 5:
+            raise Exception("The minimum dimensions available are 5, please enter values greater than this")
+        return width, height
 
 #A cell is a currently active cell in the game,
 #represented by a zero character at some x and
@@ -63,150 +197,27 @@ class Cell:
         self.x = x
         self.y = y
 
-    def fill(self):
+    def fill(self, stdscr):
         try:
             stdscr.addch( self.y, self.x, ord('0') )
         except curses.error:
             pass
 
-#Adding a cell will ensure the set condition
-#of disallowing duplicates
-def addCell(x,y,cells):
-    for cell in cells:
-        if cell.x == x and cell.y == y:
-            return cells
-    cells.append(Cell(x,y))
-    return cells
-
-def delCell(x,y,cells):
-    for cell in cells:
-        if cell.x == x and cell.y == y:
-            cells.remove(cell)
-            return cells
-    return cells
-
-#Finds adjacent alive cells
-def adjacentCells(cell, cells):
-    adj = []
-    for other in cells:
-        dx = math.fabs(cell.x-other.x)
-        dy = math.fabs(cell.y-other.y)
-        if (dx <= 1) & (dy <= 1): adj.append(other)
-    for other in adj:
-        if other.x == cell.x and other.y == cell.y:
-            adj.remove(other)
-    return adj
-
-#Finds adjacent dead points
-#TODO: This could really just be surrounding elements not
-#      in adjacentCells
-def adjacentPoints(cell, cells):
-    dead = []
-    candidates =[(x,y) 
-                for x in [cell.x-1, cell.x, cell.x+1] if (x > 0) & (x < width-1)
-                for y in [cell.y-1, cell.y, cell.y+1] if (y > 0) & (y < height-1)
-                if (x,y) != (cell.x, cell.y)
-                ]
-    cellPos = []
-    for other in cells:
-         cellPos.append((other.x, other.y))
-    dead = difference(candidates, cellPos)
-    return dead  
-  
-#The set difference of two lists a nd b
-#(assuming the lists meet set conditions of
-# uniqueness)
-def difference(a,b):
-    b = set(b)
-    return [aa for aa in a if aa not in b]            
-    
-#The main portion of life, a single step through the
-#reproduction routine
-def step(cells):
-    dead = []
-    toRemove = []
-    toAdd = []
-    for cell in cells:
-        dead.extend(adjacentPoints(cell, cells))
-    for (x,y) in dead:
-        candidate = Cell(x,y)
-        adjs = adjacentCells(candidate, cells)
-        bordering = len(adjs)
-        if bordering == 3: toAdd.append(candidate)
-    for cell in cells:
-        adjs = adjacentCells(cell, cells)
-        bordering = len(adjs)
-        if (bordering < 2) or (bordering > 3): toRemove.append(cell)
-    for cell in toRemove:
-        cells.remove(cell)
-    for cell in set(toAdd):
-        cells = addCell(cell.x, cell.y, cells)
-    fillCells(cells) 
-    return cells 
-      
-#Used for manipulating the terminal cursor for cell placement
-def mv_term_cursor(ux, uy):
-        #stdscr.addch( uy, ux, ord('.'))
-        stdscr.move(uy,ux)
-
-
-ux = 0
-uy = 0
-width = 50
-height = 30
 
 if len(argv) > 1 and len(argv) != 3:
     print "Incorrect quantity of arguments provided; please provide only width and height (i.e. both or neither)"
     print "Using defaults: width 50 height 30"
     time.sleep(3)
+    w = 50
+    h = 30
 elif len(argv) == 3:
     this, w, h = argv
-    try:
-        width = int(w)
-        height = int(h)
-    except:
-        print "Failed to interpret arguments as integers"
-        print "Using defaults: width 50 height 30"
-        width = 50
-        height = 30 
-        time.sleep(3)
-    if width < 5 or height < 5:
-        print "The minimum dimensions available are 5, please enter values greater than this"
-        print "Using defaults: width 50 height 30"
-        width = 50
-        height = 30
-        time.sleep(3)
-
+else:
+    w = 50
+    h = 30   
 
 if __name__ == "__main__":
-    stdscr = curses.initscr()
-    startup()
-    cells = []
-    generation = 0
-    nodelay = 0
-    while 1:
-        background(generation, nodelay)
-        fillCells(cells)
-        mv_term_cursor(ux, uy)
-        #by default a blocking function
-        #when [r] is pressed we move to
-        #a timeout period of a tenth of 
-        #a second for animation. 
-        c = stdscr.getch()
+    c = Life(w, h)
+    c.run()
 
-        if c == ord('s') or nodelay:
-            cells = step(cells)
-            generation = generation+1
-        if c == ord('r'):
-            if nodelay: stdscr.timeout(-1) 
-            else:       stdscr.timeout(75)
-            nodelay = nodelay ^ 1
-        elif c == ord('n'):         cells = addCell(ux, uy, cells)
-        elif c == ord('d'):         cells = delCell(ux, uy, cells)
-        elif c == curses.KEY_RIGHT: ux = ux + 1 if ux < width-1 else ux
-        elif c == curses.KEY_LEFT:  ux = ux - 1 if ux > 0 else ux
-        elif c == curses.KEY_UP:    uy = uy - 1 if uy > 0 else uy
-        elif c == curses.KEY_DOWN:  uy = uy + 1 if uy < height-1 else uy
-        elif c == ord('q'):
-            break
-    shutdown() 
+     
--- tests.py
+++ tests.py
@@ -1,8 +1,17 @@
 import life
 import sys
 
+#utility
+def containsCellAt(x,y,cells):
+    for cell in cells:
+        if cell.x == x and cell.y == y:
+            return True
+    return False
+
+
 def adjacency():
     print "Testing adjacency"
+    model = life.Life(50,50)
     cells = []
     sys.stdout.write("Populating...")
     for i in xrange(0,10):
@@ -11,7 +20,7 @@ def adjacency():
     sys.stdout.write("\n")
     failed = 0 
     for cell in cells:
-        adj = life.adjacentCells(cell, cells)
+        adj = model.adjacentCells(cell, cells)
         bordering = len(adj)
         if bordering != 2 and not (cell.x == 0 or cell.x == 9):
             print "Test failed on cell: ", cell.x, cell.y
@@ -21,9 +30,10 @@ def adjacency():
 
 def adjacentDeadPoints():
     print "Testing for adjacent dead points" 
+    model = life.Life(50,50)
     cell = life.Cell(10,10)
     cells = [cell]
-    points = life.adjacentPoints(cell, cells)
+    points = model.adjacentPoints(cell, cells)
     if len(points) != 8:
         print "Test failed, expected 8 adjacent points but found: ", str(len(points))
         return 0
@@ -31,7 +41,7 @@ def adjacentDeadPoints():
         print "Test passed with 8 adjacent points"
     print "Adding cell"
     cells.append(life.Cell(10,11))
-    points = life.adjacentPoints(cell, cells)
+    points = model.adjacentPoints(cell, cells)
     if len(points) != 7:
         print "Test failed, expected 7 adjacent points but found: ", str(len(points))        
         return 0
@@ -41,10 +51,11 @@ def adjacentDeadPoints():
 
 def testDeath():
     print "Test singleton cell death"
+    model = life.Life(50,50)
     cells = []
     cells.append(life.Cell(10,10))
     try:
-        cells = life.step(cells)
+        cells = model.step(cells)
     except: #stdscr is not defined
         pass
     if len(cells) != 0:
@@ -56,6 +67,7 @@ def testDeath():
 
 def testSpawn():
     print "Testing cell spawning"
+    model = life.Life(50,50)
     cells = []
     cells.append(life.Cell(10,10))
     cells.append(life.Cell(11,10))
@@ -63,7 +75,7 @@ def testSpawn():
     #   00
     #   0  <-- should spawn at 11,11  
     try:
-        cells = life.step(cells)
+        cells = model.step(cells)
     except: #stdcr is not defined
         pass
     if len(cells) != 4:
@@ -84,6 +96,7 @@ def testSpawn():
 
 def testBlinker():
     print "Testing the 'blinker'"
+    model = life.Life(50,50)
     cells = []
     cells.append(life.Cell(10,10))
     cells.append(life.Cell(10,11))
@@ -92,7 +105,7 @@ def testBlinker():
     for cell in cells:
          print "("+str(cell.x)+","+str(cell.y)+")"
     try:
-         cells = life.step(cells)
+         cells = model.step(cells)
     except:
         pass
     print "Cells after step:"
@@ -110,17 +123,43 @@ def testBlinker():
     print "Test passed"
     return 1
 
-def containsCellAt(x,y,cells):
-    for cell in cells:
-        if cell.x == x and cell.y == y:
-            return True
-    return False
+def testWindowDimensions():
+    print "Test negative dimensions"
+    passed = 0
+    try:
+        model = life.Life(-1,10)
+        model.run()
+        
+    except:
+        print "Test passed"
+        passed = passed + 1
+    
+    try:
+        print "Test dimensions less than limit (5,5)"
+        model = life.Life(4, 3)
+        
+    except:
+        print "Test passed"
+        passed = passed + 1
+
+    try:
+        print "Test dimensions that are valid"
+        model = life.Life(20,20)
+        passed = passed + 1
+    except:
+        print "Test failed"
+        
+    print "Dimension tests completed with " + str(passed) + " of 3 successful"
+    return 1 if passed == 3 else 0
 
 def runTests():
-    tests = [adjacency,adjacentDeadPoints,testDeath,testSpawn,testBlinker]
+    tests = [adjacency,adjacentDeadPoints,testDeath,testSpawn,testBlinker, testWindowDimensions]
     passed = 0
     for func in tests:
         passed = passed + func()
         print "================================================"
     print "Tests completed with "+str(passed)+" of "+str(len(tests))+" successful"
-runTests()
+
+if __name__ == "__main__":
+    runTests()
+ 
